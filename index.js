657c4a2ebb2830bd040940b234f2f96f187eefdc
CrystalMcCorkle <crystallmccorkle@gmail.com>
2021-06-22 15:13:51 -0500

    added a 404 not found route

--- index.js
+++ index.js
@@ -4,26 +4,49 @@ const express = require('express');
 const app = express();
 //require npm morgan
 const morgan = require('morgan');
+const { nextTick } = require('process');
 
 //tells express that on every single request, use the middleware called 'morgan'
 app.use(morgan('dev'));
+
 app.use((req, res, next) => {
-    console.log("This is my first middleware!");
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
     next();
 });
-app.use((req, res, next) => {
-    console.log("This is my second middleware!");
+
+app.use('/dogs', (req, res, next) => {
+    console.log("I LOVE DOGS!");
     next();
-});
+})
+
+// app.use((req, res, next) => {
+//     console.log("This is my first middleware!");
+//     next();
+// });
+// app.use((req, res, next) => {
+//     console.log("This is my second middleware!");
+//     next();
+// });
 
 
 app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('HOME PAGE!'); 
 });
 
 app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`)
     res.send('WOOOOOOF!'); 
 });
+
+//setting up a 404 route
+//then could render back a nice 404 not found template 
+//needs to be at end of file like this 
+app.use((req, res) => {
+    res.status(404).send("NOT FOUND!"); 
+});
+
 
 app.listen(3000, () => {
     console.log('App is running on port 3000!')
